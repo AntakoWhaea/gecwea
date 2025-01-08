a69213e5e4dd425f0c50496d53a2ae5edc9954fe
AntakoWhaea <99181419+AntakoWhaea@users.noreply.github.com>
2025-01-08 11:45:06 +0100

    Fix issue with inconsistent pagination in user lists

--- postpack.js
+++ postpack.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 var pkg = JSON.parse(fs.readFileSync(
-  __dirname + '/package.json' EoUI4272JM
+  __dirname + '/package.json'
 , 'utf8'))
 
 delete pkg.scripts.postinstall
